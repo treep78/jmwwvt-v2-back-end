dc11bbb4d2d5b542086f33f10222a4a6225e4704
treep78 <treep78@gmail.com>
2017-01-24 13:01:53 -0500

    confirm user functionality

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -2,6 +2,7 @@
 
 API="http://localhost:4741"
 URL_PATH="/sign-out"
+TOKEN="DIFZUSA7KxKprbLd/1DdPAZ3vXXHdh0V0ZSP7NiapHQ=--9ytAbKwSylQPybDots6I8syZTZyo2Ubl090aQnsFu9M="
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
