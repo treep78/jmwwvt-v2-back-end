416b23c0e794fa1cf46dbc64e6aa0f76b68b4b85
treep78 <treep78@gmail.com>
2017-02-27 17:53:58 -0500

    remove ability to create new users by removing the new user route

--- config/routes.js
+++ config/routes.js
@@ -11,7 +11,7 @@ module.exports = require('lib/wiring/routes')
 .resources('images')
 
 // users of the app have special requirements
-.post('/sign-up', 'users#signup')
+// .post('/sign-up', 'users#signup')
 .post('/sign-in', 'users#signin')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
